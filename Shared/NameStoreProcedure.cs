ad5e92533a17fac1c599a81402e4dd6cc1ebfa54
Jordy Avila <avilajordy71@gmail.com>
2025-07-12 17:25:38 -0500

    feat: Añadir métodos para manejo de XML y procedimientos SQL

---
+++ Shared/NameStoreProcedure.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiFlota.Shared
+{
+    public class NameStoreProcedure
+    {
+        public const string SP_GetName1 = "GetMantenimientos";
+        public const string SP_SetName1 = "SetMantenimientos";
+        public const string SP_GetNameUsuario = "GetUsuario";
+    }
+}
